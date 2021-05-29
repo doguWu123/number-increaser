d7f33fd3f7c6bc9ebfc489e22f76ac3685311a6d
debugger333X <75965411+debugger333X@users.noreply.github.com>
2021-05-29 16:05:59 +0530

    added function and basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles.css";
+import React, { useState } from "react";
+import {} from "react-dom";
+import "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>You clicked {count} times</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
